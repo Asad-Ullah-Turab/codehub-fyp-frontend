b90a1553118c440aa02f93764db27c42cd8872d0
israrahmad <israrahmad2004.aa@gmail.com>
2025-10-28 17:40:00 +0500

    Add: whycodehub section

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,10 +1,12 @@
 import Hero from "./Components/Hero";
 import StartJourney from "./Components/StartJourney";
+import WhyCodeHub from "./Components/WhyCodeHub";
 
 const HomePage = () => {
   return (
     <div>
       <Hero />
+      <WhyCodeHub />
       <StartJourney />
     </div>
   );
